7ab13e85b3ccc7e74139dd50207b9d68625de29a
diegocostares <diegocostar@uc.cl>
2023-10-20 21:42:03 -0300

    feat: add logo + ico

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
+import logo from "./assets/logo.svg";
+
 export default function App() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-blue-200 to-blue-600">
       <header className="bg-blue-900 text-blue-200 p-2 mb-4 w-full fixed top-0 left-0 z-10 flex justify-between items-center">
-        <h1 className="text-center font-bold text-xl">CuposSmart</h1>
+        <img src={logo} alt="CupoSmart Logo" className="h-12 w-auto mr-4" />
+        <h1 className="text-center font-bold text-xl">CupoSmart</h1>
       </header>
 
       <footer className="text-center bg-blue-900 text-yellow-200 p-2 w-full">
-        <p className="font-semibold">CuposSmart © 2023</p>
+        <p className="font-semibold">CupoSmart © 2023</p>
       </footer>
     </main>
   );
